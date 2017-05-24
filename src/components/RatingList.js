03a044686e03df3f65471216a4549e2c7b1c2b22
Jocelyn David <external.z17jdavi>
2017-05-24 13:59:37 +0200

    add axios to call ws

--- src/components/RatingList.js
+++ src/components/RatingList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import axios from 'axios';
+// cmd D in simulator to debug
 class RatingList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount');
+    //debugger;
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
+  }
   render() {
     return (
       <View>
